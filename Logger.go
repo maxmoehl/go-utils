a82347494d0b1d1a91dadd7ee68b63290841afe6
Maximilian Moehl <maximilian.moehl@gmail.com>
2020-05-25 10:20:54 +0200

    made logger more graceful, introduced middleware for go http handlers

--- Logger.go
+++ Logger.go
@@ -9,6 +9,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
 	"net/http"
+	"net/url"
 	"time"
 )
 
@@ -24,11 +25,21 @@ var (
 )
 
 // This function returns a middleware that will write logs to the log service
-func LoggingHandler() gin.HandlerFunc {
+func GinLoggingMiddleware() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		log("logger was used", Info)
 		ctx.Next()
 	}
+}
+
+func RouterMiddleWare(inner http.Handler, name string) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+
+		inner.ServeHTTP(w, r)
+
+		log(fmt.Sprintf("%s\t%s\t%s", r.Method, r.RequestURI, time.Since(start)), Info)
+	})
 }
 
 type LogMessage struct {
@@ -43,9 +54,6 @@ func log(message interface{}, severity string) {
 	if application == "" {
 		panic("application string not set")
 	}
-	if LogServiceUrl == "" {
-		panic("log service url string not set")
-	}
 	res, _ := json.Marshal(LogMessage{
 		Id:          uuid.New(),
 		TimeStamp:   time.Now().Unix(),
@@ -54,12 +62,14 @@ func log(message interface{}, severity string) {
 		Severity:    severity,
 	})
 	fmt.Println(string(res))
-	req, _ := http.NewRequest("POST", LogServiceUrl, bytes.NewReader(res))
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		panic(err)
-	} else if resp.StatusCode != http.StatusOK {
-		panic(resp.StatusCode)
+	if parsedUrl, err := url.Parse(LogServiceUrl); err == nil {
+		req, _ := http.NewRequest("POST", parsedUrl.String(), bytes.NewReader(res))
+		resp, err := http.DefaultClient.Do(req)
+		if err != nil {
+			panic(err)
+		} else if resp.StatusCode != http.StatusOK {
+			panic(resp.StatusCode)
+		}
 	}
 }
 

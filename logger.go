80960375fe8981ee81836dbc51c9bc0bc9f22f96
Maximilian Moehl <maximilian.moehl@sap.com>
2020-07-13 15:42:37 +0200

    introduce new way to write logs (+see below)
    + minor changes in documentation#
    + write to console if a request fails
    - removed panics on failing http request

--- logger.go
+++ logger.go
@@ -7,6 +7,7 @@ import (
 	"github.com/google/uuid"
 	"net/http"
 	"net/url"
+	"strconv"
 	"time"
 )
 
@@ -19,7 +20,47 @@ const (
 var (
 	application   string
 	LogServiceUrl string
+	LoggerInfo    chan interface{}
+	LoggerWarn    chan interface{}
+	LoggerError   chan interface{}
 )
+
+type LogMessage struct {
+	Id          uuid.UUID   `json:"id" bson:"id"`
+	Severity    string      `json:"severity" bson:"severity"`
+	TimeStamp   int64       `json:"timestamp" bson:"timestamp"`
+	Application string      `json:"application" bson:"application"`
+	Content     interface{} `json:"content" bson:"content"`
+}
+
+// Instead of writing logs using the goroutine that is currently executing
+// we will have a dedicated go routine that will handle logs that are being
+// sent to a channel. This should be a major performance improvement since
+// sending a request takes rather long.
+//
+// For backwards compatibility reasons the old functions
+// will stay the same for now.
+func init() {
+	LoggerInfo = make(chan interface{}, 100)
+	LoggerWarn = make(chan interface{}, 100)
+	LoggerError = make(chan interface{}, 100)
+	go logger()
+}
+
+// This function listens to the three different channels where log messages can be sent
+// by doing so,
+func logger() {
+	for {
+		select {
+		case info := <-LoggerInfo:
+			LogInfo(info)
+		case warning := <-LoggerWarn:
+			LogWarning(warning)
+		case err := <-LoggerError:
+			LogError(err)
+		}
+	}
+}
 
 // This can be used by go http servers that are following the standard signature to
 // log some statistics about the requests
@@ -36,14 +77,6 @@ func RouterMiddleWare(inner http.Handler) http.Handler {
 			"duration":      time.Since(start),
 		})
 	})
-}
-
-type LogMessage struct {
-	Id          uuid.UUID   `json:"id" bson:"id"`
-	Severity    string      `json:"severity" bson:"severity"`
-	TimeStamp   int64       `json:"timestamp" bson:"timestamp"`
-	Application string      `json:"application" bson:"application"`
-	Content     interface{} `json:"content" bson:"content"`
 }
 
 // Writes a log to the console but also tries to send the log to a logging server
@@ -64,24 +97,24 @@ func log(content interface{}, severity string) {
 		req, _ := http.NewRequest("POST", parsedUrl.String(), bytes.NewReader(res))
 		resp, err := http.DefaultClient.Do(req)
 		if err != nil {
-			panic(err)
+			fmt.Println("Error sending request: " + err.Error())
 		} else if resp.StatusCode != http.StatusOK {
-			panic(resp.StatusCode)
+			fmt.Println("Received non 200 status code: " + strconv.Itoa(resp.StatusCode))
 		}
 	}
 }
 
-// Writes a log entry with an info severity. See log() for more details
+// Writes a log entry with severity: info. See log() for more details
 func LogInfo(content interface{}) {
 	log(content, SeverityInfo)
 }
 
-// Writes a log entry with an warning severity. See log() for more details
+// Writes a log entry with severity: warning. See log() for more details
 func LogWarning(content interface{}) {
 	log(content, SeverityWarning)
 }
 
-// Writes a log entry with an error severity. See log() for more details
+// Writes a log entry with severity: error. See log() for more details
 func LogError(content interface{}) {
 	log(content, SeverityError)
 }
